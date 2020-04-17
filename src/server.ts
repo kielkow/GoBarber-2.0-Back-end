e7f9b6721b5ec5e8e2d17ac512203b7474023607
kielkow <matheuskiel@fiorifer.com.br>
2020-04-17 02:25:54 -0300

    ESLint

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,9 @@
+/* eslint-disable no-console */
 import express from 'express';
 
 const app = express();
 
-app.get('/', (request, response) => {
-  return response.json({ message: 'Hello World' });
-});
+app.get('/', (request, response) => response.json({ message: 'Hello World' }));
 
 app.listen(3333, () => {
   console.log('👨‍💻 Server started on port 3333!');
