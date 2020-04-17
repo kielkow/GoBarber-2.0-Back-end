b154f85461d957a7dbddedce858d577421c4d063
kielkow <matheuskiel@fiorifer.com.br>
2020-04-17 11:42:01 -0300

    Import TS files

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,10 @@
 /* eslint-disable no-console */
 import express from 'express';
+import routes from './routes';
 
 const app = express();
 
-app.get('/', (request, response) => response.json({ message: 'Hello World' }));
+app.use(routes);
 
 app.listen(3333, () => {
   console.log('👨‍💻 Server started on port 3333!');
