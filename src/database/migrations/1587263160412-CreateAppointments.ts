1cc000b5b284ecbd0170604520253a31db08a1c2
kielkow <matheuskiel@fiorifer.com.br>
2020-04-19 22:26:06 -0300

    Relationship between models

--- src/database/migrations/1587263160412-CreateAppointments.ts
+++ src/database/migrations/1587263160412-CreateAppointments.ts
@@ -9,7 +9,7 @@ export default class CreateAppointments1587263160412
         columns: [
           {
             name: 'id',
-            type: 'varchar',
+            type: 'uuid',
             isPrimary: true,
             generationStrategy: 'uuid',
             default: 'uuid_generate_v4()',
