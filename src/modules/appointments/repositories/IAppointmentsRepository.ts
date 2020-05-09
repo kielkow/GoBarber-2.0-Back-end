52accca7c424c27b4ad53d57282b4b0ad7fdf91e
kielkow <matheuskiel@fiorifer.com.br>
2020-05-09 14:18:03 -0300

    Rewriting Repositories

--- src/modules/appointments/repositories/IAppointmentsRepository.ts
+++ src/modules/appointments/repositories/IAppointmentsRepository.ts
@@ -1,5 +1,7 @@
 import Appointment from '../infra/typeorm/entities/Appointment';
+import ICreateAppointmentSTO from '../dtos/ICreateAppointmentDTO';
 
 export default interface IAppointmentsRepository {
+  create(data: ICreateAppointmentSTO): Promise<Appointment>;
   findByDate(date: Date): Promise<Appointment | undefined>;
 }
