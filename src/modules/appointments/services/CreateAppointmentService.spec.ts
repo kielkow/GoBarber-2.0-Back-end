03f20062a80cd5dfaac795ad08b7e3e59559c857
kielkow <matheuskiel@fiorifer.com.br>
2020-05-12 01:46:43 -0300

    Create fake repository to create first appointment test

--- src/modules/appointments/services/CreateAppointmentService.spec.ts
+++ src/modules/appointments/services/CreateAppointmentService.spec.ts
@@ -1,7 +1,21 @@
+import FakeAppointmentsRepository from '../repositories/fakes/FakeAppointmentsRepository';
 import CreateAppointmentService from './CreateAppointmentService';
 
 describe('CreateAppointment', () => {
-  it('should be able to create a new appointment', () => {});
+  it('should be able to create a new appointment', async () => {
+    const fakeAppointmentsRepository = new FakeAppointmentsRepository();
+    const createAppointment = new CreateAppointmentService(
+      fakeAppointmentsRepository,
+    );
+
+    const appointmet = await createAppointment.execute({
+      date: new Date(),
+      provider_id: '123123123',
+    });
+
+    expect(appointmet).toHaveProperty('id');
+    expect(appointmet.provider_id).toBe('123123123');
+  });
 
   // it('should not be able to create two appointments on the same time', () => {
   //   expect(1 + 2).toBe(3);
