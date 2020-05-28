cd9b243cd56266fc3fe020c4b71a9926d05db4a9
kielkow <matheuskiel@fiorifer.com.br>
2020-05-28 18:07:18 -0300

    Filter appointments by month

--- src/modules/appointments/repositories/IAppointmentsRepository.ts
+++ src/modules/appointments/repositories/IAppointmentsRepository.ts
@@ -1,7 +1,11 @@
 import Appointment from '../infra/typeorm/entities/Appointment';
 import ICreateAppointmentSTO from '../dtos/ICreateAppointmentDTO';
+import IFindALlInMonthFromProviderDTO from '../dtos/IFindALlInMonthFromProviderDTO';
 
 export default interface IAppointmentsRepository {
   create(data: ICreateAppointmentSTO): Promise<Appointment>;
   findByDate(date: Date): Promise<Appointment | undefined>;
+  findALlInMonthFromProvider(
+    data: IFindALlInMonthFromProviderDTO,
+  ): Promise<Appointment[]>;
 }
